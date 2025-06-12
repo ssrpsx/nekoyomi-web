98ca3ca650b7f1fcaf618d13cb4dafc39184b248
Sarawut Ponsan <phaiza237@gmail.com>
2025-06-12 21:55:24 +0700

    Update page : login/register

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
 import React from 'react'
 import Navbar from './components/Navbar/navbar'
-import SlideImage from './components/SlideHome/SlideImage'
 import Bg from './components/BG/background'
+import SlideImage from './components/SlideHome/SlideImage'
+import LoginAndRegister from './components/auth/auth'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 function App() {
   return (
-    <>
-      <Bg/>
-      <Navbar/>
-      <SlideImage/>
-    </>
+    <BrowserRouter>
+      <Bg />
+      <Navbar />
+      <div className='pt-24 w-full'></div>
+      <Routes>
+        <Route path='/' element={<SlideImage />}/>
+        <Route path='/Login' element={<LoginAndRegister />}/>
+        <Route path='/Register' element={<LoginAndRegister />}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
