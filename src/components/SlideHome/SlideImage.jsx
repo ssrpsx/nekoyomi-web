843d070ebf32babf377e4f5b7a3a5c4ceeafb721
Sarawut Ponsan <phaiza237@gmail.com>
2025-06-23 20:06:51 +0700

    update slideimage

--- src/components/SlideHome/SlideImage.jsx
+++ src/components/SlideHome/SlideImage.jsx
@@ -17,63 +17,63 @@ const data = [
         id : 1,
         name : "The Return of the Disaster Class Hero",
         desciption : "เรื่องย่อ : เมื่อฮีโร่ระดับต่ำที่สุด แบค ยูซอล ถูกหักหลังโดยเหล่าฮีโร่คนอื่น เขากลับมาอีกครั้งในฐานะผู้ที่จะแก้แค้นพวกทรยศเหล่านั้น และเผยความจริงเกี่ยวกับเทพและโลกที่ถูกทำลาย เพื่อยุติวงจรที่ผิดพลาด",
-        link : "/#",
+        link : "/anime/The-Return-of-the-Disaster-Class-Hero/page/home",
         img : Image1
     },
     {
         id : 2,
         name : "Swordmaster is Youngest Son",
         desciption : "เรื่องย่อ : จิน โรุนเดล — ลูกชายคนสุดท้องของตระกูลนักดาบผู้ยิ่งใหญ่ ถูกมองข้ามเพราะไม่มีพลังเวท เขาได้โอกาสกลับมาเกิดใหม่และตัดสินใจเปลี่ยนชะตาของตนเอง พร้อมพลิกโฉมตระกูลและโลกแห่งดาบและเวทมนตร์",
-        link : "/#",
+        link : "/anime/Swordmasters-Youngest-Son/page/home",
         img : Image2
     },
     {
         id : 3,
         name : "Return of the Frozen Player",
         desciption : "เรื่องย่อ : ผู้เล่นระดับ S ที่ถูกแช่แข็งหลังปราบบอสขั้นสุดท้าย กลับฟื้นขึ้นมาในอีกหลายปีต่อมา พบว่าโลกและระบบเกมได้เปลี่ยนไปหมด เขาต้องต่อสู้เพื่อทวงตำแหน่งและความจริงกลับคืนมา",
-        link : "/#",
+        link : "/anime/Return-of-the-Frozen-Player/page/home",
         img : Image3
     },
     {
         id : 4,
         name : "Return of the Flowery Mountain Sect",
         desciption : "เรื่องย่อ : ผู้นำแห่งนิกายฮวาซานในอดีต ฟื้นคืนชีพมาในยุคใหม่ที่นิกายของเขาล่มสลาย เขาจะก่อตั้งมันขึ้นใหม่และนำชื่อเสียงของ ดอกไม้แห่งภูเขา กลับคืนสู่ยุทธภพ",
-        link : "/#",
+        link : "/anime/Return-of-the-Flowery-Mountain-Sect/page/home",
         img : Image4
     },
     {
         id : 5,
         name : "Mercenary Enrollment",
         desciption : "เรื่องย่อ : ยูอิจิน รอดชีวิตจากอุบัติเหตุเครื่องบินตกและกลายเป็นทหารรับจ้าง หลังจากใช้ชีวิตในสนามรบมานาน เขากลับมาใช้ชีวิตวัยเรียนอีกครั้ง…แต่ยังคงทักษะทหารไว้ พร้อมปกป้องครอบครัวและคนที่รักจากทุกภัยคุกคาม",
-        link : "/#",
+        link : "/anime/Mercenary-Enrollment/page/home",
         img : Image5
     },
     {
         id : 6,
         name : "Life of a Magic Academy Mage",
         desciption : "เรื่องย่อ : เรื่องราวของเด็กชายที่เกิดมาในโลกเวทมนตร์ และได้รับโอกาสเรียนที่สถาบันเวทที่ดีที่สุด ชีวิตของเขาจะเปลี่ยนไปอย่างไร เมื่อความลับบางอย่างเริ่มถูกเปิดเผย?",
-        link : "/#",
+        link : "/anime/Life-of-a-Magic-Academy-Mage/page/home",
         img : Image6
     },
     {
         id : 7,
         name : "The Infinite Mage",
         desciption : "เรื่องย่อ : เด็กชายที่เกิดในหมู่บ้านยากจนใฝ่ฝันจะเป็นจอมเวท แม้จะไร้พรสวรรค์ เขากลับมีบางสิ่งที่ไม่มีใครคาดคิด การเรียนรู้ที่ไร้ขีดจำกัดจะทำให้เขาทะลุขอบเขตของเวทมนตร์ที่มนุษย์เคยรู้จัก",
-        link : "/#",
+        link : "/anime/Life-of-a-Magic-Academy-Mage/page/home",
         img : Image7
     },
     {
         id : 8,
         name : "Duke Pendragon: Master of the White Dragon Archives",
         desciption : "เรื่องย่อ : นักรบผู้ยิ่งใหญ่ที่ได้รับมอบหมายให้ปกป้องความลับของมังกรขาว หลังจากย้อนเวลากลับไป เขาจะใช้ความรู้จากอนาคตเพื่อปกป้องอาณาจักรและหยุดยั้งหายนะที่จะมาถึง",
-        link : "/#",
+        link : "/anime/Duke-Pendragon/page/home",
         img : Image8
     },
     {
         id : 9,
         name : "Damn Reincarnation",
         desciption : "เรื่องย่อ : ฮีโร่ผู้เสียสละตัวเองเพื่อหยุดจอมมาร กลับชาติมาเกิดใหม่พร้อมกับความทรงจำเดิม คราวนี้ เขาจะไม่ยอมแพ้ต่อโชคชะตาอีก และจะกลายเป็นวีรบุรุษที่แท้จริงในโลกใบนี้",
-        link : "/#",
+        link : "/anime/Damn-Reincarnation/page/home",
         img : Image9
     },
 ]
