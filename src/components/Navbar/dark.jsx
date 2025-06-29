d2d683e144576cb27f464241358eb954200f5905
Sarawut Ponsan <phaiza237@gmail.com>
2025-06-29 23:04:49 +0700

    Update dark.jsx

--- src/components/Navbar/dark.jsx
+++ src/components/Navbar/dark.jsx
@@ -20,7 +20,9 @@ function DarkModeToggle() {
 
   return (
     <div onClick={() => setDarkmode(!darkmode)} className='w-full group pl-4 pr-3 sm:px-4 py-2 bg-white rounded-full cursor-pointer flex gap-2 items-center dark:bg-gray-800 duration-200'>
-      {!darkmode ? (<MdOutlineLightMode className='text-black text-xl ml-auto text-center' />) : (<MdOutlineDarkMode className='text-white text-xl ml-auto' />)}
+      {!darkmode ?
+        (<MdOutlineLightMode className='text-black text-xl ml-auto text-center' />)
+        : (<MdOutlineDarkMode className='text-white text-xl ml-auto' />)}
       <span className='sm:hidden font-medium text-sm mr-auto'>โหมด</span>
     </div>
   )
